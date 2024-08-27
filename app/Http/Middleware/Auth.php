5dfd00d965cd8eb63d924243003bc873955ab8f3
Orifjon-github <avazbekoripov94@gmail.com>
2024-08-27 13:59:53 +0500

    api for admins

--- app/Http/Middleware/Auth.php
+++ app/Http/Middleware/Auth.php
@@ -12,20 +12,20 @@ class Auth
     public function handle(Request $request, Closure $next)
     {
         $request->headers->set('Accept', 'application/json');
-        $token = $request->header('Authorization');
-        if (!$token) {
-            $this->error(__('Unauthorized'), 401);
-        }
-        $ip = $request->ip();
-        if (!in_array($ip, config('auth.ips'))) {
-            $this->error(__("Unauthorized: $ip"), 401);
-        }
-        $client = config('auth.clients')[$token] ?? null;
-        if (!$client) {
-            $this->error(__('Unauthorized'), 401);
-        }
-        $request->headers->set('Client', $client);
-        $request->headers->set('RequestCode', uniqid());
+//        $token = $request->header('Authorization');
+//        if (!$token) {
+//            $this->error(__('Unauthorized'), 401);
+//        }
+//        $ip = $request->ip();
+//        if (!in_array($ip, config('auth.ips'))) {
+//            $this->error(__("Unauthorized: $ip"), 401);
+//        }
+//        $client = config('auth.clients')[$token] ?? null;
+//        if (!$client) {
+//            $this->error(__('Unauthorized'), 401);
+//        }
+//        $request->headers->set('Client', $client);
+//        $request->headers->set('RequestCode', uniqid());
 
         return $next($request);
     }
