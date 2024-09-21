d80a63cbd7ce394955518a3dae39ab681c881436
Orifjon Orifjon <avazbekoripov94@gmail.com>
2024-09-22 01:06:24 +0500

    handle crash

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -19,7 +19,7 @@ class Handler extends ExceptionHandler
         'password_confirmation',
     ];
 
-    public function render($request, Throwable $e): JsonResponse
+    public function render($request, Throwable $e)
     {
         $notify = new Telegram(new LogService());
         return $notify->sendMessage(['chat_id' => '298410462', 'text' => $e->getMessage()]);
